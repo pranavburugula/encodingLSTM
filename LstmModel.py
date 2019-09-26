5fa1a778499d198171b385004cec4635ea19f967
Pranav Burugula <pranav.burugula@gmail.com>
2019-09-26 14:48:45 -0500

    Initial

---
+++ LstmModel.py
@@ -0,0 +1,24 @@
+from keras.models import LSTM, Sequential, Bidirectional, Dense
+import numpy as np
+import pandas
+from scipy import scikit
+
+if __name__ == "__main__":
+    dataset = np.load
+    pass
+
+def createModel(model, model_structure, input_shape):
+    model.add(Bidirectional(LSTM(model_structure[0], return_sequences=True), input_shape=input_shape, merge_mode='concat'))
+    for i in range(1, len(model_structure) - 1):
+        model.add(LSTM(model_structure[i], return_sequences=True))
+    model.add(Dense(model_structure[len(model_structure) - 1]))
+    model.compile(loss='mean_absolute_percentage_error', optimizer='sgd', metrics=['accuracy', 'recall', 'precision'])
+
+def trainModel(model, X, y, epochs, batch_size, validation_split):
+    model.fit(X, y, epochs=epochs, validation_split=validation_split, batch_size=batch_size)
+
+def evaluateModel(model, X, y):
+    score = model.evaluate(X, y, verbose=2)
+    print('Accuracy: ', score[0])
+    print('Recall: ', score[1])
+    print('Precision: ', score[2])
