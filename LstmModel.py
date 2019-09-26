c2e54cdc630f64e7b90c89b1a87a0f870e93496e
Pranav Burugula <pranav.burugula@gmail.com>
2019-09-26 18:10:14 -0500

    C:/ProgramData/Anaconda3/Scripts/activate

--- LstmModel.py
+++ LstmModel.py
@@ -1,13 +1,21 @@
-from keras.models import LSTM, Sequential, Bidirectional, Dense
+from keras.models import Sequential
+from keras.layers import LSTM, Bidirectional, Dense
 import numpy as np
 import pandas
 from sklearn.datasets import fetch_20newsgroups_vectorized
+import os
 
 if __name__ == "__main__":
-    train = fetch_20newsgroups_vectorized(subset='train')
-    test = fetch_20newsgroups_vectorized(subset='test')
+    categories = ['alt.atheism', 'comp.graphics', 'comp.os.ms-windows.misc', 'comp.sys.ibm.pc.hardware', 
+    'comp.sys.mac.hardware', 'comp.windows.x', 'misc.forsale', 'rec.autos', 'rec.motorcycles', 'rec.sport.baseball', 
+    'rec.sport.hockey', 'sci.crypt', 'sci.electronics']
+    train = fetch_20newsgroups_vectorized(subset='train', categories=categories)
+    test = fetch_20newsgroups_vectorized(subset='test', categories=categories)
     model = Sequential()
-    createModel(model, [])
+    createModel(model, [200, 400, 20], train.data.shape)
+    trainModel(model, train.data, train.target)
+    saveModel(model, 'C:\\Users\\pb8xe\\Documents\\C3SR\\encodingLSTM\\models', '20newsgroupsLSTM')
+    evaluateModel(model, test.data, test.target)
     pass
 
 def createModel(model, model_structure, input_shape):
@@ -25,3 +33,16 @@ def evaluateModel(model, X, y):
     print('Accuracy: ', score[0])
     print('Recall: ', score[1])
     print('Precision: ', score[2])
+
+def saveModel(model, outputDir, filePrefix):
+        outFilename_model = filePrefix + '.json'
+        outFilepath = os.path.join(outputDir, outFilename_model)
+        # serialize model to JSON
+        model_json = model.to_json()
+        with open(outFilepath, "w") as json_file:
+            json_file.write(model_json)
+        # serialize weights to HDF5
+        outFilename_weights = filePrefix + '.h5'
+        outFilepath = os.path.join(outputDir, outFilename_weights)
+        model.save_weights(outFilepath)
+        print("Saved model to disk")
